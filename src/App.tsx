030a950d0608d3e6d9c57dced28768494f82afd4
max <max.assis@chatguru.com.br>
2023-08-17 19:24:14 -0300

    feat: feito o modal inicial

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import classNames from 'classnames';
 
 const socket = io("http://localhost:3333");
 
@@ -13,15 +14,19 @@ type Msg = {
 };
 
 function App() {
+  const [error, setError] = useState(false);
   const [message, setMessage] = useState("");
   const [chat, setchat] = useState<Msg[]>([]);
+  const [modalInput, setModalInput] = useState("");
+  const [name, setName] = useState("");
+  const [modal, setModal] = useState(false);
 
   useEffect(() => {
-    // setHeight(elementRef.current.clientHeight);
     function receivedMessage(message: Msg) {
       const newMessage: Msg = message;
       console.log(newMessage);
       setchat([...chat, newMessage]);
+      setMessage("");
     }
 
     socket.on("msgToClient", (message: Msg) => {
@@ -29,17 +34,52 @@ function App() {
     });
   }, [chat]);
 
+  const modalSpan = classNames({
+    'chat__modal-error' : true,
+    'chat__modal-error--show' : error && !modalInput
+  });
+
+  const modalShow = classNames({
+    'chat__modal' : true,
+    'chat__modal--hidden' : modal
+  })
+
   function sendMessage(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    e.preventDefault();
+    e.preventDefault();    
+    if(message.trim() === "") return
     socket.emit("msgToServer", { user, message });
+  }
+
+  function checkModalError(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
+    !modalInput ? setError(false) : setName(modalInput);
+    
+    if(!modalInput) {
+      setError(false);
+    } else {
+      setName(modalInput)
+      setModal(true);
+    }
   }
 
   return (
     <>
       <div className="external-container">
+        <div className={modalShow}>
+            <div className="chat__modal-content">
+              <h3>Digite seu nome:</h3>
+              <form>
+              <input type="text" value={modalInput} onChange={(e) => setModalInput(e.target.value)}/>
+              <span className={modalSpan} >Nome e obrigatorio</span>
+              {name}
+              <button onClick={(e) => checkModalError(e)}>Enviar</button>
+              </form>
+            </div>
+        </div>
+
         <div className="chat__container">
           <div className="chat__users">
-            <h2 className="chat__name">Max</h2>
+            <h2 className="chat__name">{name}</h2>
           </div>
 
           <div className="chat__content-wrapper">
