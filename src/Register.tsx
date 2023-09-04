a43bc716d56d4ee31a6b1ee91da017e0adcc0bec
max <max.assis@chatguru.com.br>
2023-09-04 20:16:36 -0300

    feat: feito a requisição do registro

--- src/Register.tsx
+++ src/Register.tsx
@@ -1,15 +1,39 @@
 import "./styles/login.scss";
 import back from "./assets/imgs/back.avif";
 import "./styles/login.scss";
-import register from "./assets/imgs/register.avif";
+import  userImg from "./assets/imgs/register.avif";
 import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useLocalStorage } from 'react-use';
+import { useNavigate } from 'react-router-dom'
+import { z } from "zod"
+
+
+const schema = z.object({
+  name: z.string().nonempty("Campo obrigatorio").transform((name) => name.toLowerCase().trim()),
+  password: z.string().min(3, "Mínimo de 3 caracteres").transform((password) => password.trim()),
+});
+
+type RegisterSchema = z.infer<typeof schema>
+
+// const decode = (token: string) =>
+//     decodeURIComponent(
+//         atob(token.split('.')[1].replace('-', '+').replace('_', '/'))
+//             .split('')
+//             .map((c) => `%${('00' + c.charCodeAt(0).toString(16)).slice(-2)}`)
+//             .join('')
+//     );
 
 export default function Register() {
   const [file, setFile] = useState("");
   const [name, setName] = useState("");
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const values = useRef<any>(File);
+  const { register, handleSubmit, formState: { errors }} = useForm<RegisterSchema>({ resolver: zodResolver(schema) });
+  const [, setToken] = useLocalStorage('max-token', '');
+  const navigate = useNavigate();
 
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if(e.target.files) {
@@ -19,12 +43,34 @@ export default function Register() {
   setName(e.target.files![0].name);
 }
 
-  const submitForm = async () => {
-    if (!values.current.file) {
-      return false;
-    }
+  function onSubmit(dt: {name: string, password: string} ) {
+    fetch("http://localhost:3333/auth/register", {
+      method: "POST",
+      body: JSON.stringify(dt),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        if(!json.token) {
+          alert(json.message)
+          return
+        }
 
+        setToken(json.token)
+        const token = json.token
 
+        if(values.current.file) {     
+         uploadAvatar(token)
+         return
+        }
+
+        navigate('/chat')
+      })
+      .catch((err) => console.log(err));
+    
+  }
+
+  const uploadAvatar = async (tk: string) => {
     const formData = new FormData();
     formData.append('file', values.current.file);
     
@@ -33,13 +79,14 @@ export default function Register() {
         method: 'POST',
         body: formData,
         headers: {
-          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEyLCJuYW1lIjoidGVzdGUxIiwiaWF0IjoxNjkzNTA3NTMxLCJleHAiOjE3MjUwNjUxMzF9.eOBxbP7BqC3QEYf1CuXrUPJimOhy5fz7S0VMUk54594"
+          "Authorization": "Bearer " + tk
         }
       });
       if (!response.ok) {
         throw new Error(response.statusText);
       }
       console.log(response);
+      navigate('/chat')
     } catch (err) {
       console.log(err);
     }
@@ -48,7 +95,7 @@ export default function Register() {
   return (
     <section className="login" style={{ backgroundImage: `url(${back})` }}>
       <section className="register__wrapper">
-        <img src={register} />
+        <img src={userImg} />
 
         <div className="register__content-wrapper">
           <div className="register__title">
@@ -64,18 +111,20 @@ export default function Register() {
             </div>
           </div>
 
-          <form className="register__inputs">
+          <form className="register__inputs" onSubmit={handleSubmit(onSubmit)}>
             <div
               className="register__input-wrapper"
               style={{ marginBlockStart: "0" }}
             >
               <span>Nome</span>
-              <input type="text" />
+              <input type="text" {...register("name")} />
+              {errors.password && <h6 className="login__error">{errors.name?.message}</h6>}
             </div>
 
             <div className="register__input-wrapper">
               <span>Senha</span>
-              <input type="text" />
+              <input type="password" {...register("password")} />
+              {errors.password && <h6 className="login__error">{errors.password?.message}</h6>}
             </div>
 
             <div className="register__avatar-wrapper">
@@ -98,7 +147,7 @@ export default function Register() {
               </span>
             </div>
 
-            <button type="button" className="register__button" onClick={() => submitForm()}>
+            <button className="register__button">
               Crie sua conta
             </button>
           </form>
