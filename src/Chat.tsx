ab6a36d9e296a2a4e1ff9caeb84fde8f1aab1665
max <max.assis@chatguru.com.br>
2023-09-13 10:06:52 -0300

    feat: setando o render

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -10,9 +10,8 @@ import i18n from "@emoji-mart/data/i18n/pt.json";
 import ContentEditable, {ContentEditableEvent} from "react-controlled-contenteditable";
 import { useLocalStorage } from 'react-use';
 
-// const socket = io("https://max-chat-f7uh.onrender.com");
-const socket = io("http://localhost:3333");
-//const user = uuidv4();
+const socket = io("https://max-chat-f7uh.onrender.com");
+//const socket = io("http://localhost:3333");
 
 const decode = (token: string): string =>
     decodeURIComponent(
